16ad3996514926285ec761ba86e21ae860d74f04
Claire Roberts <clairefinnie@gmail.com>
2022-11-07 14:42:35 -0500

    add redux

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 import { Text } from 'react-native';
 import { Avatar, Card, ListItem } from 'react-native-elements';
 import { ScrollView } from 'react-native-gesture-handler';
-import { PARTNERS } from '../shared/partners';
 
 const Mission = () => {
 	return (
@@ -23,16 +23,17 @@ const Mission = () => {
 };
 
 const AboutScreen = () => {
-	const [partners, setPartners] = useState(PARTNERS);
+	const partners = useSelector((state) => state.partners);
+
 	return (
 		<ScrollView>
 			<Mission />
 			<Card>
 				<Card.Title>Community Partners</Card.Title>
 				<Card.Divider />
-				{partners.map((partner) => (
+				{partners.partnersArray.map((partner) => (
 					<ListItem key={partner.id}>
-						<Avatar rounded source={partner.image} />
+						<Avatar rounded source={{ uri: baseUrl + partner.image }} />
 						<ListItem.Content>
 							<ListItem.Title>{partner.name}</ListItem.Title>
 							<ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
---
+++ shared/baseUrl.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'http://192.168.0.59:3001/';
+
+// check current IP address if any issues on different networks or after reconnecting to current network
